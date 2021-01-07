b812d87c126cbcc374242197f5dd7b400a6c435c
and_web <andtext007@gmail.com>
2021-01-07 19:14:23 +0200

    install composer for package and project

---
+++ task_2/my_project/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'bdc4cbf1ef0977376259eaf3ced24bb82ebe87ce',
+    'name' => 'andrey/my_project',
+  ),
+  'versions' => 
+  array (
+    'andrey/my_project' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'bdc4cbf1ef0977376259eaf3ced24bb82ebe87ce',
+    ),
+  ),
+);
